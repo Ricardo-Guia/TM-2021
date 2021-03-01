494be20dd48b327c6ed046cce9b54ed4f9b81d31
Lupinheiro <40212718+lupinheiro@users.noreply.github.com>
2021-03-01 16:29:05 +0000

    Exercicio1

---
+++ Aula 2/exercicio1/LuisAlonso-17506/script.js
@@ -0,0 +1,8 @@
+function readTextInput(){
+
+    var inputText = document.getElementById("inputText").value;
+    if (inputText){
+        document.getElementById("viewText").innerText = inputText
+    }
+
+}
