8cd50e898e49288b39b1e80656db050de56853cd
thalisson1980 <thalisson1980@gmail.com>
2021-03-01 15:23:13 +0000

    Correçao

--- Aula 2/exercicio2/Template/script.js
+++ Aula 2/exercicio2/Template/script.js
@@ -2,20 +2,20 @@
 
 let addedNumbers = [];
 
-function pushArray(){
+function pushArray() {
     const number = document.getElementById("numbers").value;
     addedNumbers.push(number);
 
-     document.getElementById("arrayNumbers").innerText=addedNumbers.toString();
+    document.getElementById("arrayNumbers").innerText = addedNumbers.toString();
 
 }
 
-function calculateMaxNumber(){
-    if(addedNumbers.length>=5){
-        const maxNumber = addedNumbers.reduce((a,b) => Math.max(a,b));
+function calculateMaxNumber() {
+    if (addedNumbers.length >= 5) {
+        const maxNumber = addedNumbers.reduce((a, b) => Math.max(a, b));
 
-        document.getElementById("maxNumber").innerText=maxNumber;
-    }else{
+        document.getElementById("maxNumber").innerText = maxNumber;
+    } else {
         alert("tem de inserir pelo menos 5 numeros")
     }
 
