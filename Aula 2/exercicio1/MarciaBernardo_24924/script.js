895189555e73fbcdf5daa54424bdf98f3dfeeb15
Márcia Regina Bernardo <79754243+Marcia-Bernardo@users.noreply.github.com>
2021-03-01 23:28:18 +0000

    Exercicio1 Correction

--- Aula 2/exercicio1/MarciaBernardo_24924/script.js
+++ Aula 2/exercicio1/MarciaBernardo_24924/script.js
@@ -1,9 +1,9 @@
 // Your JS Script here
-function readInput(){
-    const text = document.getElementById( 'texto').value;
-    if(!text){
-        alert("Escreva texto no input")
+function readInput() {
+    const text = document.getElementById('texto').value;
+    if (!text) {
+        alert("Escreva texto no input");
     } else {
-        document.getElementById('textoFromInput').innerText = text
+        document.getElementById('textoFromInput').innerText = text;
     }
 }
