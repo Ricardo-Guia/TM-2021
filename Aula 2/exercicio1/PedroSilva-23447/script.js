8b4d8fa39f02b52560235bf56d7c5b1c7d9a84ff
Shnip1906 <silva.pedro@ipvc.pt>
2021-03-01 16:29:17 +0000

    Exercicio1

---
+++ Aula 2/exercicio1/PedroSilva-23447/script.js
@@ -0,0 +1,10 @@
+// Your JS Script here
+
+function readTextInput(){
+
+    const inputText = document.getElementById("inputText").value;
+
+    if(inputText) {
+        document.getElementById("viewText").innerText = inputText;
+    }
+}
