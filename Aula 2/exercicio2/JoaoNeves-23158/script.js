4aa09e4be34ba6127823ae0c0d4d15c955b97dc5
joa0neves <unfit4winter@gmail.com>
2021-03-01 16:49:16 +0000

    ex2

--- Aula 2/exercicio2/JoaoNeves-23158/script.js
+++ Aula 2/exercicio2/JoaoNeves-23158/script.js
@@ -1,9 +1,22 @@
 // Your JS Script here
-function readText() {
-    const inputText = document.getElementById("inputText").value;
-    console.log(inputText);
-    if (inputText) {
-        document.getElementById("viewText").innerText = inputText;
+numbers=[];
+
+function readNumber() {
+    const input = document.getElementById("inputText").value;
+    if (input){
+        numbers.push(input);
     }
 
+    document.getElementById("viewNumbers").innerText=document.getElementById("viewNumbers").innerText + input+', '
+}
+
+function calculate(){
+    if(numbers.length>5){
+        var max = numbers.reduce(function(a, b) {
+            return Math.max(a, b);
+        });
+        document.getElementById("viewNumber").innerText='Maior Número: '+max;
+    }else{
+        document.getElementById("viewNumber").innerText='São necessários pelo menos 5 números'
+    }
 }
