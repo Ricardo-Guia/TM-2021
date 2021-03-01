2d02a93724c97d75eedef19645a5e81ad3fb6b4f
extremelf <luis.paulo.6.1.2001@gmail.com>
2021-03-01 17:47:45 +0000

    Exercício 1 correção

--- Aula 2/exercicio2/Luís Fernandes-23439/script.js
+++ Aula 2/exercicio2/Luís Fernandes-23439/script.js
@@ -1,1 +1,11 @@
 // Your JS Script here
+
+function readInputFromText(){
+    const textFromInput = document.getElementById("inputText").value
+    if(textFromInput){
+        document.getElementById("viewText").innerText = textFromInput
+    }
+    else{
+        alert("Escreva algo no input")
+    }
+}
