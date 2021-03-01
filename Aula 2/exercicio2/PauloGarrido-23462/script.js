0cd6835517f6c6e451bafd6afae911ae1f581467
PauloGarrido17 <79467740+PauloGarrido17@users.noreply.github.com>
2021-03-01 16:54:14 +0000

    Exercicio2

---
+++ Aula 2/exercicio2/PauloGarrido-23462/script.js
@@ -0,0 +1,18 @@
+// Your JS Script here
+
+let numbersArray = []
+function addNumber(){
+  const number = document.getElementById("numberInput").value
+  numbersArray.push(number)
+  document.getElementById("numbersAdded").innerHTML = numbersArray.join(', ')
+}
+
+function calculate(){
+  if(numbersArray.length >= 5) {
+    const maxNumber = numbersArray.reduce(function (a, b) {
+      return Math.max(a, b);
+    });
+    const maxNumberV2 = numbersArray.reduce((a, b)=>Math.max(a,b));
+    document.getElementById("maxNumberViewer").innerText = maxNumber + " é igual a "+maxNumberV2
+  }
+}
