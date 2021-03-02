f2d8691eb98753fe2c2b7d9cf4669f4096b6d6d7
Gabriel Sousa <guigas78@gmail.com>
2021-03-02 12:44:46 +0000

    Exercicio 2

---
+++ Aula 2/exercicio2/GabrielSousa-21519/script.js
@@ -0,0 +1,22 @@
+const numberInput = document.getElementById("numberInput");
+const numberDisplay = document.getElementById("numberDisplay");
+const calcDisplay = document.getElementById("calcDisplay");
+let numberArray = [];
+
+document.getElementById("buttonAddNumbers").addEventListener('click', () => {
+  !numberInput.value ? alert("Insira um numero!") : (() => {
+    numberArray.push(parseInt(numberInput.value))
+    numberDisplay.innerText += numberInput.value + ","
+  })()
+})
+
+document.getElementById("buttonCalcNumbers").addEventListener("click", () => {
+  numberArray.length < 5 ? alert("Inserir no minimo 5 numeros!") : calcDisplay.textContent = "Maior Numero: " + numberArray.reduce((a, b) => {
+    return Math.max(a, b);
+  })
+})
+
+
+
+
+
