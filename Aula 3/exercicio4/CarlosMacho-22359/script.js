bd137c1660f636c83eef17872f3549a2ab69f406
carlosmacho <carlosmacho@ipvc.pt>
2021-03-08 17:01:38 +0000

    Aula 3 exercicio4

--- Aula 3/exercicio4/CarlosMacho-22359/script.js
+++ Aula 3/exercicio4/CarlosMacho-22359/script.js
@@ -1,1 +1,17 @@
 // Your JS Script here
+let imgCounter = 0;
+
+function addImage() {
+    imgCounter++;
+    let img = document.createElement("img")
+    img.src="https://placeimg.com/250/150" + imgCounter
+
+    const imagens = document.getElementById("imageContainer")
+    imagens.appendChild(img)
+
+}
+
+function delImage() {
+    const imagens = document.getElementById("imageContainer")
+    imagens.children[0].remove()
+}
