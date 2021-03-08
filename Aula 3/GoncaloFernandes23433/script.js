f93c2dc7c85f3286203489188d30f524b820db67
goncalomf <65426418+goncalomf@users.noreply.github.com>
2021-03-08 16:56:38 +0000

    exercício3

---
+++ Aula 3/GoncaloFernandes23433/script.js
@@ -0,0 +1,37 @@
+// Your JS Script here
+
+window.onload = function () {
+    const tdElements = document.getElementsByTagName("td")
+    for(let td of tdElements) {
+        td.onclick = function (element) {
+            const result = prompt("Novo Valor?")
+            element.target.style.backgroundColor= "green"
+            element.target.innerText = result;
+        }
+    }
+}
+
+readAtributtes = () =>  {
+
+    const element = document.getElementsByTagName("a")[0]
+    console.log(element.attributes)
+
+    const list = document.getElementsByTagName("ul")[0]
+
+
+
+    for(let att of element.attributes) {
+        const  liElement = document.createElement("li")
+
+        liElement.innerText = `${att.name}: ${att.value}`;
+
+        list.appendChild(liElement)
+    }
+}
+    changeColorRed = () => {
+
+        const titles = document.getElementsByClassName("titulo")
+        for(let title of titles) {
+            title.style.color = "red";
+        }
+    }
