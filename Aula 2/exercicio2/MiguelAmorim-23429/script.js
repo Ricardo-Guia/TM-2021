57e0407f3b8c8bec358ed1f2b5f879d424dc88a1
MiguelAmorim23429 <79472249+MiguelAmorim23429@users.noreply.github.com>
2021-03-09 14:29:56 +0000

    Aula 2 exercicio2

--- Aula 2/exercicio2/MiguelAmorim-23429/script.js
+++ Aula 2/exercicio2/MiguelAmorim-23429/script.js
@@ -1,5 +1,20 @@
 // Your JS Script here
 
-function listAttributes() {
-  const
+let numbersArray = []
+
+function addNumberFromInput() {
+  const textFromInput = document.getElementById("inputNumber").value;
+  if (textFromInput) {
+    numbersArray.push(textFromInput)
+    document.getElementById("viewerNumber").innerText = numbersArray.join(", ");
+  } else {
+    alert("Escreva algo no input");
+  }
+}
+
+function calculateMaxNumberFromArray() {
+  if (numbersArray.length >= 5) {
+    const maxNumber = numbersArray.reduce((a, b) => Math.max(a, b));
+    document.getElementById("maxNumber").innerText = maxNumber;
+  }
 }
