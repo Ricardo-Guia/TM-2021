1e12195f62d51b5a7c464da869f8c81a74f56438
Gabriel <guigas78@gmail.com>
2021-03-09 14:31:27 +0000

    Corrected Ex.3

--- Aula 3/Exercicio 3/GabrielSousa-21519/script.js
+++ Aula 3/Exercicio 3/GabrielSousa-21519/script.js
@@ -1,1 +1,54 @@
 // Your JS Script here
+
+window.onload = () => {
+  const ulList = document.getElementsByTagName('ul')
+  /* One way of doing it
+    getAttributes = (linkInfo) => {
+      let linhaId = document.createElement('li')
+      linhaId.append('ID: ' + linkInfo.id)
+
+      let linhaHref = document.createElement('li')
+      linhaHref.append('href: ' + linkInfo.href)
+
+      let linhaTarget = document.createElement('li')
+      linhaTarget.append('target: ' + linkInfo.target)
+
+      let linhaType = document.createElement('li')
+      linhaType.append('type: ' + linkInfo.type)
+
+      ulList[0].append(linhaId, linhaHref, linhaTarget, linhaType)
+   */
+  getAttributes = (linkInfo) => {
+    for (let i = 0; i < linkInfo.attributes.length; i++) {
+      /* This one OR
+      let dummy = document.createElement('li')
+      dummy.append(linkInfo.attributes[i].localName + ": "+ linkInfo.attributes[i].value)
+      ulList[0].append(dummy) */
+
+      ulList[0].append((() => {
+        let dummy = document.createElement('li')
+        dummy.append(linkInfo.attributes[i].localName + ": " + linkInfo.attributes[i].value)
+        return dummy
+      })())
+
+    }
+  }
+
+  changeColor = () => {
+    let titulos = document.getElementsByClassName('titulo')
+    for (let i = 0; i < titulos.length; i++) {
+      titulos[i].style.color = "red"
+    }
+  }
+
+  for (let elem of document.getElementsByTagName('td')) {
+    elem.onclick = (clickedEvent) => {
+      const response = prompt('Novo valor para a celula')
+      const targetElement = clickedEvent.target
+      targetElement.style.background = 'green'
+      targetElement.innerText = response
+    }
+  }
+}
+
+
