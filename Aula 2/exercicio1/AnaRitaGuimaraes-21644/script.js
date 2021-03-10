3d36267e1116010f6840ad0f3500cdf2e2e9bb0e
Ana Guimarães <aritaguimaraes@ipvc.pt>
2021-03-10 15:52:28 +0000

    Exercicio1

---
+++ Aula 2/exercicio1/AnaRitaGuimaraes-21644/script.js
@@ -0,0 +1,10 @@
+// Your JS Script here
+
+function readImputFromText() {
+    const textFromImput = document.getElementById("imputText").value
+    if (textFromImput) {
+        document.getElementById("viewerText").innerText = textFromImput
+    } else {
+        alert("Escreva algo no imput");
+    }
+}
