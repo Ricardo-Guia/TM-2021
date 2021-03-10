509c3e5aefde1f8a00f65675b77812c43f94bf7e
Ana Guimarães <aritaguimaraes@ipvc.pt>
2021-03-10 15:54:43 +0000

    Correção Exercicio1

--- Aula 2/exercicio1/AnaRitaGuimaraes-21644/script.js
+++ Aula 2/exercicio1/AnaRitaGuimaraes-21644/script.js
@@ -1,9 +1,9 @@
 // Your JS Script here
 
 function readImputFromText() {
-    const textFromImput = document.getElementById("imputText").value
+    const textFromImput = document.getElementById("imputText").value;
     if (textFromImput) {
-        document.getElementById("viewerText").innerText = textFromImput
+        document.getElementById("viewerText").innerText = textFromImput;
     } else {
         alert("Escreva algo no imput");
     }
