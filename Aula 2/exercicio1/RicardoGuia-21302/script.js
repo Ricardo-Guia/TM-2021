b1f73789904efbe3bacb9123dcbed7be576f26f6
Gabriel <guigas78@gmail.com>
2021-03-15 14:20:57 +0000

    Deleted unnecessary files

--- Aula 2/exercicio1/RicardoGuia-21302/script.js
+++
@@ -1,8 +0,0 @@
-// Your JS Script here
-
-function readTextInput() {
-	const inputText = document.getElementById("inputText").value;
-	if (inputText) {
-		document.getElementById("viewText").innerText = inputText;
-	}
-}
