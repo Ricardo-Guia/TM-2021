6cc01bf5beb2208cde5e7f48ea20e62f562040ff
Ana Guimarães <aritaguimaraes@ipvc.pt>
2021-03-18 02:26:33 +0000

    Exercício 3 Aula 3

---
+++ Aula 3/exercicio3/AnaRitaGuimarães-21644/script.js
@@ -0,0 +1,33 @@
+// Your JS Script here
+
+window.onload = function (element) {
+
+    const tdElements = document.getElementsByTagName("td");
+
+    for (let td of tdElements) {
+        td.onclick = (event)=>{
+            const clickedElem = event.target;
+            clickedElem.innerText = prompt("Change Value to? ");
+            clickedElem.style.backgroundColor = "#99e599"
+        }
+    }
+};
+
+function listAttributes() {
+    const elementAnchor = document.getElementsByTagName("a")[0]
+    const elementList = document.getElementsByTagName("ul")[0]
+    elementList.innerText = '';
+
+    for (let attr of elementAnchor.attributes) {
+        const li = document.createElement("li")
+        li.innerText = `${attr.name}: ${attr.value}`
+        elementList.appendChild(li)
+    }
+}
+
+changeColorOfTitles = () => {
+    const elementsByClass = document.getElementsByClassName("titulo")
+    for (let element of elementsByClass) {
+        element.style.color = "red"
+    }
+}
