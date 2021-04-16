4bd504abbabb451a42312e3ff58b7e9e8833cebd
extremelf <luis.paulo.6.1.2001@gmail.com>
2021-04-16 17:15:54 +0100

    Aula 7

--- Aula 7/Exercicio/course/examples/03/game.js
+++ Aula 7/Exercicio/course/examples/03/game.js
@@ -41,7 +41,11 @@ class preloadGame extends Phaser.Scene{
     preload(){
         this.load.image("background", "assets/sprites/background.png");
         this.load.spritesheet("cloud", "assets/sprites/cloud.png", {
+
+            // frame width, in pixels
             frameWidth: 256,
+
+            // frame height, in pixels
             frameHeight: 256
         });
     }
@@ -56,6 +60,8 @@ class playGame extends Phaser.Scene{
     create(){
         this.addBackground();
         this.addClouds();
+
+
     }
     addBackground(){
         let background = this.add.sprite(-50, -50, "background");
@@ -64,25 +70,55 @@ class playGame extends Phaser.Scene{
         background.displayHeight = game.config.height + 100;
     }
     addClouds(){
+
+        // clouds is the amount of cloud images needed to cover the entire game width
         let clouds = Math.ceil(game.config.width / 128);
+
+        // we store clouds in cloudsArray array
         let cloudsArray = [];
+
+        // this loop is executed twice because we want two rows of clouds
         for(let i = 0; i <= 1; i ++){
+
+            // this loop is executed "cloud" times
             for(let j = 0; j <= clouds; j ++){
+
+                // add cloud image to the game with a bit of randomization in its position
                 let cloud = this.add.sprite(128 * j + Phaser.Math.Between(-10, 10), game.config.height + i * 32 + Phaser.Math.Between(-10, 10), "cloud");
+
+                // set cloud frame
                 cloud.setFrame(i);
+
+                // insert the cloud in cloudsArray array
                 cloudsArray.push(cloud);
             }
         }
+
+        // tween the clouds
         this.tweens.add({
+
+            // array containing game objects to tween: all clouds
             targets: cloudsArray,
+
+            // properties to tween
             props: {
+
+                // x property (horizontal position)
                 x: {
+
+                    // how do we change the value?
                     value: {
+
+                        // once we have the position at the end of the tween...
                         getEnd: function(target, key, value){
+
+                            // ... move it for a little amount of pixels
                             return target.x + Phaser.Math.Between(-10, 10)
                         }
                     }
                 },
+
+                // same concept applied to y property (vertical position)
                 y: {
                     value: {
                         getEnd: function(target, key, value){
@@ -91,9 +127,16 @@ class playGame extends Phaser.Scene{
                     }
                 }
             },
+
+            // duration of the tween, in milliseconds
             duration: 3000,
+
+            // how many times are we repeating the tween? -1 = repeat forever
             repeat: -1,
+
+            // yoyo effect: execute the tween back and forth
             yoyo: true
         });
     }
-};
+
+}
